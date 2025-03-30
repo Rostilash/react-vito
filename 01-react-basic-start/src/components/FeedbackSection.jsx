4a751af397d5ebc9ccfd7837c1e9d79ce434f7a2
Rostislav <acrossfear2022@gmail.com>
2025-03-30 16:45:47 +0300

    Two way binding in form

--- 01-react-basic-start/src/components/FeedbackSection.jsx
+++ 01-react-basic-start/src/components/FeedbackSection.jsx
@@ -1,7 +1,36 @@
+import Button from "./Button/Button.jsx";
+import { useState } from "react";
+
 export default function FeedbackSection() {
+  const [name, setName] = useState("");
+  const [reason, setReason] = useState("help");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <section>
       <h3> Зворотній зв'язок</h3>
+      <form>
+        <label htmlFor="name"> Ваше Імя </label>
+        <input type="text" className="control" value={name} onChange={handleNameChange}></input>
+
+        <label htmlFor="name"> Причина звернення </label>
+        <select id="reason" className="control" value={reason} onChange={(event) => setReason(event.target.value)}>
+          <option value="error">Помилка</option>
+          <option value="help">Потрібна допомога</option>
+          <option value="suggest">Пропозиція</option>
+        </select>
+
+        <pre>
+          Name: {name}
+          <br />
+          Reason: {reason}
+        </pre>
+
+        <Button> Надіслати </Button>
+      </form>
     </section>
   );
 }
