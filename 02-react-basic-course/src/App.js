73e99b80395f88416406b8b2749a5e99153d3af6
Rostislav <acrossfear2022@gmail.com>
2025-03-31 18:34:34 +0300

    Mapping the incoming array.

--- 02-react-basic-course/src/App.js
+++ 02-react-basic-course/src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter.jsx";
 import ClassCounter from "./components/ClassCounter.jsx";
+import "./styles/app.css";
+import { PostItem } from "./components/PostItem";
 
 export default function App() {
-  const [value, setValue] = useState("test input");
-
+  const [post, setPost] = useState([
+    {
+      id: 1,
+      title: "Javascript",
+      body: "descripton",
+    },
+    {
+      id: 2,
+      title: "Javascript 2",
+      body: "descripton",
+    },
+    {
+      id: 3,
+      title: "Javascript 3",
+      body: "descripton",
+    },
+  ]);
   return (
     <div className="App">
-      <Counter />
-      <Counter />
-      <ClassCounter />
+      {post.map((post) => (
+        <PostItem post={post} key={post.id}></PostItem>
+      ))}
     </div>
   );
 }
