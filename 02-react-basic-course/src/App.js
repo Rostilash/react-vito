a3175e906903e02f74a4d293dbe6a841abacdd81
Rostislav <acrossfear2022@gmail.com>
2025-03-31 23:29:32 +0300

    Made selector filtered by name or description

--- 02-react-basic-course/src/App.js
+++ 02-react-basic-course/src/App.js
@@ -4,25 +4,28 @@ import { PostList } from "./components/PostList.jsx";
 import { MyButton } from "./components/UI/button/MyButton";
 import { MyInput } from "./components/UI/input/MyInput";
 import { PostForm } from "./components/PostForm";
+import { MySelect } from "./components/UI/select/MySelect";
 
 export default function App() {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      title: "Javascript",
-      body: "description",
+      title: "CJavascript",
+      body: "sdescription",
     },
     {
       id: 2,
-      title: "Javascript 2",
-      body: "description",
+      title: "BJavascript 2",
+      body: "vdescription",
     },
     {
       id: 3,
-      title: "Javascript 3",
-      body: "description",
+      title: "AJavascript 3",
+      body: "adescription",
     },
   ]);
+  const [selectedSort, setSelectedSort] = useState("");
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -31,10 +34,25 @@ export default function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ margin: "1rem" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортування"
+          options={[
+            { value: "title", name: "По назві" },
+            { value: "body", name: "По опису" },
+          ]}
+        />
+      </div>
       {/* умовне відмальовування */}
       {posts.length ? (
         <PostList remove={removePost} posts={posts} title="Список Постів" />
