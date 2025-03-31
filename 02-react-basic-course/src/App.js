79ee00332c6f4f5dfae8ac42fcc85a844f8d584d
Rostislav <acrossfear2022@gmail.com>
2025-03-31 18:06:02 +0300

    work with classes but the didn't use in our days but some information for understanding.

--- 02-react-basic-course/src/App.js
+++ 02-react-basic-course/src/App.js
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter.jsx";
+import ClassCounter from "./components/ClassCounter.jsx";
 
 export default function App() {
-  const [likes, setLikes] = useState(5);
   const [value, setValue] = useState("test input");
 
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={(event) => setValue(event.target.value)}></input>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
