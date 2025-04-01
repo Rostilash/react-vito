a161cd2071d31670635ee14e52e776f8dbab420d
Rostislav <acrossfear2022@gmail.com>
2025-04-01 18:14:52 +0300

    Processing loading indications

--- 02-react-basic-course/src/App.js
+++ 02-react-basic-course/src/App.js
@@ -10,13 +10,18 @@ import { MyModal } from "./components/UI/MyModal/MyModal";
 import { usePosts } from "./hooks/usePosts.js";
 import PostService from "./components/API/PostService.jsx";
 import { Loader } from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching.js";
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -26,15 +31,6 @@ export default function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   // отримуємо props із дочірнього елемента
   const removePost = (post) => {
@@ -58,6 +54,7 @@ export default function App() {
 
       <hr style={{ margin: "1rem" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Відбулася помилка {postError}</h1>}
       {isPostsLoading ? (
         <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
           <Loader />
