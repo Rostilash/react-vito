274f47a8969439568411499e00a324a7b83a9c81
Rostislav <acrossfear2022@gmail.com>
2025-04-02 18:24:30 +0300

    Make isAuth for login page

--- 02-react-basic-course/src/components/AppRouter.jsx
+++ 02-react-basic-course/src/components/AppRouter.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { privateRoutes } from "./../router/index";
+import { privateRoutes, publicRoutes } from "./../router/index";
 
 export const AppRouter = () => {
-  return (
+  const isAuth = false;
+  return isAuth ? (
     <Routes>
       <Route path="/" element={<Navigate to="/posts" replace />} />
 
@@ -11,6 +12,17 @@ export const AppRouter = () => {
       {privateRoutes.map((route, index) => (
         <Route key={index} path={route.path} element={route.element} />
       ))}
+      <Route path="/login" element={<Navigate to="/posts" replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      {publicRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 };
--- 02-react-basic-course/src/router/index.js
+++ 02-react-basic-course/src/router/index.js
@@ -2,6 +2,7 @@ import { About } from "./../pages/About";
 import { Posts } from "./../pages/Posts";
 import { Error } from "./../pages/Error";
 import { PostIdPage } from "../pages/PostIdPage";
+import { Login } from "./../pages/Login";
 
 export const privateRoutes = [
   {
