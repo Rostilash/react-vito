90f9d67de01effbc26557a7824d9db186b0a409e
Rostislav <acrossfear2022@gmail.com>
2025-04-03 19:17:51 +0300

    react+vito+scss

---
+++ email-editor/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="layout">
+      <h1>Hello World</h1>
+      <div style={{ padding: "10" }}> Hello </div>
+      <button> Click</button>
+    </div>
+  );
+};
