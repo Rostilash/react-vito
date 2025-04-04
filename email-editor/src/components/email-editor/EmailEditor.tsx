7fb4dba4645cf6b3085552e09a3e126d94c2b74c
Rostislav <acrossfear2022@gmail.com>
2025-04-04 19:34:59 +0300

    adding parser and lucide-react for the project

--- email-editor/src/components/email-editor/EmailEditor.tsx
+++ email-editor/src/components/email-editor/EmailEditor.tsx
@@ -1,10 +1,61 @@
 import styles from "./EmailEditor.module.scss";
+import { Bold, Eraser, Italic, Underline } from "lucide-react";
+import { useState, useRef } from "react";
+import { applyStyle, TStyle } from "./apply-style";
 
 export const EmailEditor = () => {
+  const [text, setText] =
+    useState(`At the Command Line With a simple but powerful shell script called todo.sh, you can interact with todo.txt at the command line for quick and
+          easy, Unix-y access. The Todo.txt CLI supports archiving completed tasks to done.txt and priority/context tab autocompletion.`);
+
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const applyFormat = (type: TStyle) => {
+    if (!textRef.current) return;
+    const cursorStart = textRef.current?.selectionStart;
+    const cursorEnd = textRef.current?.selectionEnd;
+
+    const selectedText = text.substring(cursorStart, cursorEnd);
+    if (!selectedText) return;
+
+    const before = text.substring(0, cursorStart);
+    const after = text.substring(cursorEnd);
+
+    setText(before + applyFormat(type, selectedText) + after);
+  };
+
   return (
-    <>
+    <div>
       <h1>Email Editor</h1>
-      <div className={styles.layout}>h</div>
-    </>
+      <div className={styles.cart}>
+        <textarea
+          ref={textRef}
+          className={styles.editor}
+          spellCheck="false"
+          onClick={applyFormat}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        >
+          {text}
+        </textarea>
+        <div className={styles.actions}>
+          <div className={styles.tools}>
+            <button onClick={() => setText("")}>
+              <Eraser size={16} />
+            </button>
+            <button>
+              <Bold size={16} />
+            </button>
+            <button>
+              <Italic size={16} />
+            </button>
+            <button>
+              <Underline size={16} />
+            </button>
+          </div>
+          <button> Send now </button>
+        </div>
+      </div>
+    </div>
   );
 };
--- email-editor/src/components/email-list/EmailList.tsx
+++ email-editor/src/components/email-list/EmailList.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import style from "./EmailList.module.scss";
 
 export const EmailList = () => {
-  return <div>EmailList</div>;
+  return (
+    <div className={style.list}>
+      <div>
+        Can you add a little more detail about the steps after this? Will it start typechecking after you rename your .jsx files to .tsx? (For
+        context, I'm starting with glitch.com/~glitch-hello-react, which uses vite as default, but doesn't come with typechecking) Harsh CommentedAug
+        30, 2022 at 21:17
+      </div>
+    </div>
+  );
 };
